d2b6307060cb0dfa76d4420c4d5c7ca59a9dce01
Zsaber <Ngodinh2339@gmail.com>
2025-05-15 04:37:10 +0700

    chinh port cloud

--- run.py
+++ run.py
@@ -14,4 +14,6 @@ os.makedirs('instance', exist_ok=True)
 app = create_app()
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    port = int(os.environ.get("PORT", 8080))  # Đọc từ biến môi trường
+    socketio.run(app, host='0.0.0.0', port=port, debug=True)
+
